6a8a4dd6847b6c415f3f9b84569c8382b6d5e740
Swapnil Hota <45566012+swapnilhota@users.noreply.github.com>
2021-08-10 19:16:45 +0530

    Create 2. Third largest element.cpp

---
+++ DSA/Arrays/2. Third largest element.cpp
@@ -0,0 +1,40 @@
+/*
+Given an array of distinct elements. Find the third largest element in it. 
+
+Example 1:
+
+Input:
+N = 5
+A[] = {2,4,1,3,5}
+Output: 3
+Example 2:
+
+Input:
+N = 2
+A[] = {10,2}
+Output: -1
+Your Task:
+Complete the function thirdLargest() which takes the array a[] and the size of the array, n, as input parameters and returns the third largest element in the array. It return -1 if there are less than 3 elements in the given array.
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 ≤ N ≤ 105
+1 ≤ A[i] ≤ 105
+*/
+
+class Solution{
+  public:
+    int thirdLargest(int a[], int n)
+    {
+         if(n<3)
+         {
+             return -1;
+         }
+         
+         sort(a, a+n);
+         return a[n-3];
+    }
+
+};
